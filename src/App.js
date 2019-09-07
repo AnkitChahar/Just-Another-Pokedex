0ff787e193f4c0d143ec3f359d4a99f9f0273021
Ankit <ankitchahar11@gmail.com>
2019-09-08 00:20:30 +0530

    Added sequential request methon (Cards will load one by one), added text color method

--- src/App.js
+++ src/App.js
@@ -1,93 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import Pokemon from './Pokemon/Pokemon';
-import axios from 'axios';
-import {Grid, Container} from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import './App.css'
+import React, { useState, useEffect } from "react";
+import Pokemon from "./Pokemon/Pokemon";
+import axios from "axios";
+import { Grid, Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
 function App() {
-
-  const[pokemonState, setPokemonState] = useState({
-    pokemons:[]
+  const [pokemonState, setPokemonState] = useState({
+    pokemons: []
   });
 
-  useEffect(() => {
-    let tempState = {
-      pokemons: []
-    };
-    firstRequest(tempState, 42);
-  }, []);
+  let numOfRequests = 100;
 
-  let firstRequest = async (state, numOfPokemon) => {
-    for(let i=1;i<=numOfPokemon;i++){
-      let tempVar = {};
-      let requestArray = [];
-      requestArray.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then(response => {
-        // console.log(`First request for index ${i}`);
-        tempVar.id = i;
+  useEffect(() => {
+    if (pokemonState.pokemons.length < numOfRequests) {
+      firstRequest(pokemonState.pokemons.length + 1);
+    }
+  });
+
+  let firstRequest = async id => {
+    let tempState = [...pokemonState.pokemons];
+    let tempVar = {};
+    let requestArray = [];
+    requestArray.push(
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then(response => {
+        // console.log(`First request for index ${id}`);
+        tempVar.id = id;
         tempVar.name = response.data.name;
         tempVar.img = response.data.sprites.front_default;
-        tempVar.type = response.data.types[response.data.types.length - 1].type.name;
-      }));
-      requestArray.push(axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`).then(response => {
-        // console.log(`Second request for index ${i}`);
-        tempVar.desc = response.data.flavor_text_entries[1].flavor_text;
-        tempVar.color = response.data.color.name;
-      }));
-      await Promise.all(requestArray);
-      // console.log("Pushing in state")
-      state.pokemons.push(tempVar);
-    }
-    // console.log("Setting Pokemon State", state);
-    setPokemonState(state);
+        tempVar.type =
+          response.data.types[response.data.types.length - 1].type.name;
+      })
+    );
+    requestArray.push(
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+        .then(response => {
+          // console.log(`Second request for index ${id}`);.flavor_text
+          tempVar.desc = response.data.flavor_text_entries.filter(text => {
+            return text.language.name === "en"
+              ? text.flavour_text
+              : "No Description in English";
+          });
+          tempVar.color = response.data.color.name;
+          tempVar.gen = response.data.generation.name.split("-")[1];
+        })
+    );
+    await Promise.all(requestArray);
+    // console.log("Pushing in state")
+    tempState.push(tempVar);
+    // console.log("Setting Pokemon State", tempState);
+    setPokemonState({
+      pokemons: tempState
+    });
   };
 
-  let onScrollEvent = (e) => {
-    console.log("Scroll Now ",e);
-    // let state = pokemonState;
-    // let requestArray = [];
-    // for(let i=pokemonState.pokemons.length + 1;i<=pokemonState.pokemons.length + 10;i++){
-    //   let tempVar = {};
-    //   requestArray.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then(response => {
-    //     // console.log(`First request for index ${i}`);
-    //     tempVar.id = i;
-    //     tempVar.name = response.data.name;
-    //     tempVar.img = response.data.sprites.front_default;
-    //     tempVar.type = response.data.types[response.data.types.length - 1].type.name;
-    //   }));
-    //   requestArray.push(axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`).then(response => {
-    //     // console.log(`Second request for index ${i}`);
-    //     tempVar.desc = response.data.flavor_text_entries[1].flavor_text;
-    //     tempVar.color = response.data.color.name;
-    //   }));
-    //   // console.log("Pushing in state")
-    //   state.pokemons.push(tempVar);
-    // }
-    // Promise.all(requestArray).then(() => {
-    //   // console.log("Setting Pokemon State", state);
-    //   setPokemonState(state);
-    // })
-  }
+  let onScrollEvent = e => {
+    console.log("Scroll Now ", e);
+  };
 
   let pokemonDiv = (
     <Grid stackable columns={6}>
-      {
-        pokemonState.pokemons.map((pokemon, index) => {
-          return (
-            <Grid.Column key={pokemon.id}>
-              <Pokemon name={pokemon.name} mainImage={pokemon.img} type={pokemon.type} desc={pokemon.desc} color={pokemon.color} num={index + 1} />
-            </Grid.Column>
-          );
-        })
-      }
+      {pokemonState.pokemons.map((pokemon, index) => {
+        return (
+          <Grid.Column key={pokemon.id}>
+            <Pokemon
+              name={pokemon.name}
+              mainImage={pokemon.img}
+              type={pokemon.type}
+              desc={pokemon.desc}
+              color={pokemon.color}
+              num={index + 1}
+              gen={pokemon.gen}
+            />
+          </Grid.Column>
+        );
+      })}
     </Grid>
   );
 
   return (
     <div className="GridStyle" onScroll={onScrollEvent}>
-      <Container>
-        {pokemonDiv}
-      </Container>
+      <Container>{pokemonDiv}</Container>
     </div>
   );
 }
--- src/Pokemon/Pokemon.js
+++ src/Pokemon/Pokemon.js
@@ -1,49 +1,61 @@
-import React from 'react';
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import React from "react";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 
-function Pokemon(props) { // name ,mainImage, type, desc
+function Pokemon(props) {
+  // name ,mainImage, type, desc, gen
 
-    const mainCardStyle = {
-        marginTop: '5px',
-        marginBottom: '5px',
-        borderRadius: '8px'
-    }
+  const textColorValue = color => {
+    const isWhite = ["green", "red", "blue", "brown", "purple", "gray"];
+    return isWhite.indexOf(color) !== -1 ? "white" : "black";
+  };
 
-    const frontCardStyle = {
-        border: '2px solid black',
-        borderRadius: '8px',
-        textTransform: 'capitalize',
-        fontWeight: 'bold',
-        textAlign: 'center'
-    };
+  const mainCardStyle = {
+    marginTop: "5px",
+    marginBottom: "5px",
+    borderRadius: "8px"
+  };
 
-    const backCardStyle = {
-        border: '2px solid black',
-        borderRadius: '8px',
-        textTransform: 'capitalize',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        overflow: 'none | scroll',
-        backgroundColor: props.color,
-        color: 'white'
-    };
+  const frontCardStyle = {
+    border: "2px solid black",
+    borderRadius: "8px",
+    textTransform: "capitalize",
+    fontWeight: "bold",
+    textAlign: "center"
+  };
 
-    return(
-        <Flippy flipOnHover={false} flipOnClick={true} flipDirection="horizontal" style={mainCardStyle}>
-            <FrontSide style={frontCardStyle}>
-            <div>
-                <p>{props.name}</p>
-                <img src={props.mainImage} alt={props.name}/>
-            </div>
-            </FrontSide>
-            <BackSide style={backCardStyle}>
-            <div>
-                <p>{props.num}</p>
-                <p>{props.type}</p>
-            </div>
-            </BackSide>
-        </Flippy>
-    );
+  const backCardStyle = {
+    border: "2px solid black",
+    borderRadius: "8px",
+    textTransform: "capitalize",
+    fontWeight: "bold",
+    textAlign: "center",
+    overflow: "none | scroll",
+    backgroundColor: props.color,
+    color: textColorValue(props.color)
+  };
+
+  return (
+    <Flippy
+      flipOnHover={false}
+      flipOnClick={true}
+      flipDirection="horizontal"
+      style={mainCardStyle}
+    >
+      <FrontSide style={frontCardStyle}>
+        <div>
+          <p>{props.name}</p>
+          <img src={props.mainImage} alt={props.name} />
+        </div>
+      </FrontSide>
+      <BackSide style={backCardStyle}>
+        <div>
+          <p>ID - {props.num}</p>
+          <p>Type - {props.type}</p>
+          <p>Generation - {props.gen}</p>
+        </div>
+      </BackSide>
+    </Flippy>
+  );
 }
 
 export default Pokemon;
